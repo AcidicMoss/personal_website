fca8664b285060efcccff27cd8dc0023d1462143
Robert <robert.a.miller.410@gmail.com>
2024-04-21 22:07:54 -0500

    set up the website router and made some basic pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
+// import scss/css pages to appy the styles to the components
+import './scss/App.scss';
 
+// Do not forget to install this pakage, you may get an error at first.
+import {BrowserRouter as Router, Route, Routes, useNavigate} from 'react-router-dom';
+
+// Import the pages that we want to render
+import Home from './Home';
+import About from './About';
+
+// Our js files return components that are rendered in index.html
 function App() {
   return (
-    <div>
-      <h1>React App Basics</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
